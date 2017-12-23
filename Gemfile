d231f7da64cb75b18ff296c25ef37c60b17d1b7c
marizaswart <mariza.swart@gmail.com>
2017-12-23 14:01:26 +0100

    Generate article controller to create a new article and show the created article

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 
 group :development, :test do
+  gem 'pry-byebug'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails'
   gem 'shoulda-matchers'
---
+++ features/step_definitions/create_article_steps.rb
@@ -0,0 +1,20 @@
+Given("I visit the landing page") do
+ visit root_path
+end
+
+When("I click {string} link") do |link|
+  click_link link
+end
+
+When("I fill in {string} with {string}") do |field, value|
+  fill_in field, with: value
+end
+
+When("I click {string} button") do |button|
+  click_button button
+end
+
+Then("I should be on {string} page") do |article_title|
+  article = Article.find_by(title: article_title)
+  expect(page.current_path).to eq "/articles/#{article.id}"
+end
